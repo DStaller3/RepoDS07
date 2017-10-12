f9b89c676a39d497a40f83c6fd16b53043812d71
llullanqui <luis.1142@hotmail.com>
2017-10-12 12:51:29 -0500

    Saludo

---
+++ saludos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suma;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Administrador
+ */
+public class saludos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Escriba su nombre: ");
+        String nombre = sc.nextLine();
+        System.out.println("Hola " + nombre);
+    }
+    
+}
